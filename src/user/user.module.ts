0a2eb096ccf42aa7464b1bfacee6856ebaca60f8
phibuinek <phibuinek@gmail.com>
2024-10-03 11:51:26 +0700

    fix bug

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,28 +1,14 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { UserController } from "./user.controller";
-import * as mongoose from "mongoose";
 import { UserService } from "./user.service";
-import {
-  configureUserSchema,
-  User,
-  UserSchema,
-} from "src/auth/schemas/user.schema";
+import { User, UserSchema } from "src/auth/schemas/user.schema";
 
 @Module({
   imports: [
-    MongooseModule.forFeatureAsync([
-      {
-        name: User.name,
-        useFactory: (connection: mongoose.Connection) => {
-          configureUserSchema(connection); // Gọi hàm để cấu hình auto-increment cho ID
-          return UserSchema;
-        },
-        inject: [mongoose.Connection], // Inject kết nối mongoose
-      },
-    ]),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]), // Đăng ký model Pet
   ],
-  providers: [UserService],
   controllers: [UserController],
+  providers: [UserService],
 })
 export class UserModule {}
