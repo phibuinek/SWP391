354885545d3c114026dbf097e5fd81a4a16dc15d
phibuinek <phibuinek@gmail.com>
2024-10-12 19:47:51 +0700

    fix bug

--- src/volunteer-task/volunteer-task.module.ts
+++ src/volunteer-task/volunteer-task.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { VolunteerTaskService } from './volunteer-task.service';
 import { VolunteerTaskController } from './volunteer-task.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { VolunteerTask, VolunteerTaskSchema } from './schemas/volunteer-task.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: VolunteerTask.name, schema: VolunteerTaskSchema },
+    ]), // Đăng ký model Pet
+  ],
   controllers: [VolunteerTaskController],
   providers: [VolunteerTaskService],
 })
