24e37df2b799cabc2e02344455ec09aebba3a738
phibuinek <phibuinek@gmail.com>
2024-10-26 22:24:39 +0700

    fix bug donate

--- src/payment/payment.controller.ts
+++ src/payment/payment.controller.ts
@@ -1,18 +1,31 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { BadRequestException, Body, Controller, Post } from "@nestjs/common";
 import { PaymentService } from "./payment.service";
 
-@Controller("payment")
+@Controller('payment')
 export class PaymentController {
   constructor(private readonly paymentService: PaymentService) {}
 
   @Post()
-  async createPayment(
-    @Body() paymentDto: { amount: number; currency: string; orderId: string },
-  ) {
-    return this.paymentService.createPayment(
-      paymentDto.amount,
-      paymentDto.currency,
-      paymentDto.orderId,
-    );
+  async handlePaymentWebhook(@Body() paymentData: { amount: number; currency: string; orderId: string; clientId: string }) {
+    console.log("Payment handler", paymentData);
+
+    // Bạn có thể xác minh và sử dụng thông tin từ paymentData
+    const verifiedData = this.paymentService.verifyPaymentWebhookData(paymentData);
+
+    // Kiểm tra thông tin
+    if (["Ma giao dich thu nghiem", "VQRIO123"].includes(verifiedData.description)) {
+      return {
+        error: 0,
+        message: "Ok",
+        data: verifiedData,
+      };
+    }
+
+    return {
+      error: 0,
+      message: "Ok",
+      data: verifiedData,
+    };
   }
 }
+
